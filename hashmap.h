390584a5f896c3c95373e93ee3d9935fd51e1032
Eqke <KrazyToinik@gmail.com>
2020-11-27 21:33:57 +0700

    -

--- hashmap.h
+++ hashmap.h
@@ -110,10 +110,11 @@ public:
               bool keyIs = false;
               for (size_t i = 0; i < _keys.size(); i++)
                 {
-                  if (std::equal_to(_keys[i], it_fl->first))
+                  if (std::equal_to <const key_type>() (_keys[i], it_fl->first))
                     {
                       keyIs = true;
                       break;
+
                     }
                 }
               if (!keyIs)
@@ -157,7 +158,8 @@ public:
             bool keyIs = false;
             for (size_t i = 0; i < _keys.size(); i++)
               {
-                if (std::equal_to(_keys[i], it_fl->first))
+
+                if (std::equal_to <const key_type>() (_keys[i], it_fl->first))
                   {
                     keyIs = true;
                     break;
@@ -361,7 +363,7 @@ public:
       {
         for (auto list:_lists[hash])
           {
-            if (std::equal_to(list.first, key))
+            if (std::equal_to <const key_type>() (list.first, key))
               {
                 count_with_key++;
               }
@@ -383,7 +385,7 @@ public:
         auto it = _lists[hash].begin();
         while(it != _lists[hash].end())
           {
-            if (std::equal_to(it->first, newd.first))
+            if (std::equal_to <const key_type>() (it->first, newd.first))
               {
                 isCreated = true;
                 it->second = newd.second;
@@ -423,7 +425,7 @@ public:
         auto it = _lists[hash].begin();
         while (it != _lists[hash].end())
           {
-            if (std::equal_to(it->first, key))
+            if (std::equal_to <const key_type>() (it->first, key))
               {
                 iterator returned_it(&(*it),hash,this);
                 return returned_it;
@@ -461,7 +463,7 @@ public:
         auto it = _lists[hash].begin();
         while (it != _lists[hash].end())
           {
-            if (std::equal_to(it->first,key))
+            if (std::equal_to <const key_type>() (it->first,key))
               {
                 return it->second;
               }
@@ -484,7 +486,7 @@ public:
         auto it = _lists[hash].begin();
         while (it != _lists[hash].end())
           {
-            if (std::equal_to(it->first, key))
+            if (std::equal_to <const key_type> (it->first, key))
               {
                 return const_cast<const data_type&>(it->second);
               }
@@ -528,7 +530,7 @@ public:
         auto it = _lists[hash].begin();
         while (it != _lists[hash].end())
           {
-            if (std::equal_to(it->first, key))
+            if (std::equal_to <const key_type>() (it->first, key))
               {
                 return it->second;
               }
@@ -549,7 +551,7 @@ public:
       auto before_it = _lists[hash].before_begin();
       while (it != _lists[hash].end())
         {
-          if (std::equal_to(it->first, key))
+          if (std::equal_to <const key_type>() (it->first, key))
             {
               it++;
               _lists[hash].erase_after(before_it);
--- main.cpp
+++ main.cpp
@@ -10,36 +10,34 @@ int main()
 {
   HashMap<std::string, int> map;
   map.insert(std::pair<std::string, int> ("green",2));
-  const HashMap<std::string, int> map1;
-  std::cout << map["green"] << "\n";
-//  auto pair = map.insert(std::pair<std::string, int> ("red",2));
-//  std::cout << "status insert" << "\n";
-//  std::cout << pair.second << "\n";
-//  pair = map.insert(std::pair<std::string, int> ("red",4));
-//  std::cout << pair.second << "\n";
-//  auto it = pair.first;
+  auto pair = map.insert(std::pair<std::string, int> ("red",2));
+  std::cout << "status insert" << "\n";
+  std::cout << pair.second << "\n";
+  pair = map.insert(std::pair<std::string, int> ("red",4));
+  std::cout << pair.second << "\n";
+  auto it = pair.first;
 
-//  map.insert(std::pair<std::string, int> ("green",5));
-//  map.insert(std::pair<std::string, int> ("blue",2));
-//  map.insert(std::pair<std::string, int> ("fiolet",8));
-//  map.insert(std::pair<std::string, int> ("appl",4));
-//  std::cout << "key access" << "\n";
-//  std::cout << map["fiolet"] << " " << map.at("appl") << "\n";
-//  std::cout << "iterator 1 work" << "\n";
-//  for (auto&& [key, value] : map) {
-//        std::cout << key << " " << value << "\n";
-//    }
-//  std::cout << "delete status\n";
-//  std::cout << map.erase("green") << "\n";
-//  std::cout << "iterator 2 work" << "\n";
-//  for (auto it = map.begin(); it != map.end(); it++)
-//    {
-//      std::cout << it.operator*().first << " " << it.operator*().second << "\n";
-//    }
-//  auto find_eleme = map.find("fiolet");
-//  if (find_eleme != map.end())
-//    {
-//      std::cout << find_eleme.operator*().first << " " << find_eleme.operator*().second << "\n";
-//    }
+  map.insert(std::pair<std::string, int> ("green",5));
+  map.insert(std::pair<std::string, int> ("blue",2));
+  map.insert(std::pair<std::string, int> ("fiolet",8));
+  map.insert(std::pair<std::string, int> ("appl",4));
+  std::cout << "key access" << "\n";
+  std::cout << map["fiolet"] << " " << map.at("appl") << "\n";
+  std::cout << "iterator 1 work" << "\n";
+  for (auto&& [key, value] : map) {
+        std::cout << key << " " << value << "\n";
+    }
+  std::cout << "delete status\n";
+  std::cout << map.erase("green") << "\n";
+  std::cout << "iterator 2 work" << "\n";
+  for (auto it = map.begin(); it != map.end(); it++)
+    {
+      std::cout << it.operator*().first << " " << it.operator*().second << "\n";
+    }
+  auto find_eleme = map.find("fiolet");
+  if (find_eleme != map.end())
+    {
+      std::cout << find_eleme.operator*().first << " " << find_eleme.operator*().second << "\n";
+    }
 }
 
